1651a77c143fd4af086793f60b140d416b448b63
Mateo Ochandorena <mateochando@gmail.com>
2023-07-14 15:51:07 +0200

    Add script tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Header from '../components/Header'
-import { Inter } from 'next/font/google'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "../components/Header";
+import { Inter } from "next/font/google";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
+  return (
     <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3507918834376944"
+        crossOrigin="anonymous"
+      />
       <Header />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
